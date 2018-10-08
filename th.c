2d981017a52d0633eaca05824ef03f82f656dd71
Adam Borowski <kilobyte@angband.pl>
2018-10-08 16:03:05 +0200

    Test reading 1000 keys, sequentially in a loop, out of 1000.
    Key values are random.

--- th.c
+++ th.c
@@ -18,7 +18,7 @@ static uint64_t rnd64(unsigned int *seedp)
 }
 
 static int bad=0;
-#define CHECK(x) do if (!(x)) printf("\e[31mWRONG: \e[1m%s\e[22m at line \e[1m%d\e[22m\n", #x, __LINE__),bad=1; while (0)
+#define CHECK(x) do if (!(x)) printf("\e[31mWRONG: \e[1m%s\e[22m at line \e[1m%d\e[22m\n", #x, __LINE__),bad=1,exit(1); while (0)
 static int done=0;
 
 
@@ -112,6 +112,49 @@ static void test_read1_of_1000()
     printf("\e[F\e[40C%15lu\n", count);
 }
 
+static void* thread_read1000_of_1000(void* c)
+{
+    uint64_t count=0;
+    unsigned int seed=0;
+    while (!done)
+    {
+        uint64_t v=rnd64(&seed);
+        CHECK(hm_get(c, v) == (void*)v);
+        if (!(++count%1000))
+            seed=0;
+    }
+    return (void*)count;
+}
+
+static void test_read1000_of_1000()
+{
+    void *c = hm_new();
+    unsigned int seed=0;
+    for (int i=0; i<1000; i++)
+    {
+        uint64_t v=rnd64(&seed);
+        hm_insert(c, v, (void*)v);
+    }
+
+    pthread_t th[NTHREADS];
+    done=0;
+    for (int i=0; i<NTHREADS; i++)
+        CHECK(!pthread_create(&th[i], 0, thread_read1000_of_1000, c));
+    sleep(1);
+    done=1;
+
+    uint64_t count=0;
+    for (int i=0; i<NTHREADS; i++)
+    {
+        void* retval;
+        CHECK(!pthread_join(th[i], &retval));
+        count+=(uintptr_t)retval;
+    }
+
+    hm_delete(c);
+    printf("\e[F\e[40C%15lu\n", count);
+}
+
 static void run_test(void (*func)(void), const char *name)
 {
     printf("TEST: %s\n", name);
@@ -130,10 +173,12 @@ int main()
     TEST(read1);
     TEST(read1_of_2);
     TEST(read1_of_1000);
+    TEST(read1000_of_1000);
     HM_SELECT(cuckoo_mutex);
     printf("%s\n", hm_name);
     TEST(read1);
     TEST(read1_of_2);
     TEST(read1_of_1000);
+    TEST(read1000_of_1000);
     return 0;
 }
