f1b3281986fe23ef4799c13e42a8b813918eb698
Adam Borowski <kilobyte@angband.pl>
2018-10-08 14:39:06 +0200

    Placate a warning.

--- test.c
+++ test.c
@@ -7,8 +7,8 @@ int main()
     void *c = hm_new();
     hm_insert(c, 123, "abc");
 
-    printf("123 = %016lx\n", hm_get(c, 123));
-    printf("124 = %016lx\n", hm_get(c, 124));
+    printf("123 = %016lx\n", (uintptr_t)hm_get(c, 123));
+    printf("124 = %016lx\n", (uintptr_t)hm_get(c, 124));
 
     hm_delete(c);
     return 0;
