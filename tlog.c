4df2b690ad5da6a1fa66acde7f1f382f9c13fdc9
Adam Borowski <kilobyte@angband.pl>
2018-10-12 15:09:42 +0200

    tlog: add timestamps and a second piece of data.

--- tlog.c
+++ tlog.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include "tlog.h"
 #include "util.h"
 
-#define TLOG_SIZE (64*1048576)
+#define TLOG_SIZE (32*1048576)
 
 static struct
 {
+    uint64_t timestamp;
     uint32_t tid;
     uint32_t act;
-    uint64_t data;
+    uint64_t data1;
+    uint64_t data2;
 } tlog_data[TLOG_SIZE];
 
 static uint64_t i;
@@ -19,14 +22,23 @@ void tlog_init()
     i=0;
 }
 
-void tlog(uint32_t tid, uint32_t act, uint64_t data)
+static inline uint64_t getticks(void)
+{
+     unsigned a, d;
+     asm volatile("rdtsc" : "=a" (a), "=d" (d));
+     return ((uint64_t)a) | (((uint64_t)d) << 32);
+}
+
+void tlog(uint32_t tid, uint32_t act, uint64_t data1, uint64_t data2)
 {
     uint64_t o = util_fetch_and_add64(&i, 1);
     if (o < TLOG_SIZE)
     {
+        tlog_data[o].timestamp=getticks();
         tlog_data[o].tid=tid;
         tlog_data[o].act=act;
-        tlog_data[o].data=data;
+        tlog_data[o].data1=data1;
+        tlog_data[o].data2=data2;
     }
 }
 
@@ -41,4 +53,3 @@ void tlog_save()
     write(f, tlog_data, l*sizeof(tlog_data[0]));
     close(f);
 }
-
--- tlog.h
+++ tlog.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 void tlog_init();
-void tlog(uint32_t tid, uint32_t act, uint64_t data);
+void tlog(uint32_t tid, uint32_t act, uint64_t data1, uint64_t data2);
 void tlog_save();
