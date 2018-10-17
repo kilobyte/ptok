dc89eb33cdfac291159614da8d9ead7bbfaab8a0
Adam Borowski <kilobyte@angband.pl>
2018-10-17 12:44:31 +0200

    th: unify most of test code.

--- th.c
+++ th.c
@@ -60,7 +60,7 @@ static void* thread_read1(void* c)
     return (void*)count;
 }
 
-static void* thread_read1000_of_1000(void* c)
+static void* thread_read1000(void* c)
 {
     uint64_t count=0;
     int i=0;
@@ -135,6 +135,24 @@ static void* thread_read1_cachekiller(void* c)
     return (void*)count;
 }
 
+static void* thread_read1000_cachekiller(void* c)
+{
+    volatile uint64_t cache[CACHESIZE][8];
+    uint64_t count=0;
+    int i=0;
+    while (!done)
+    {
+        for (int k=0; k<CACHESIZE; k++)
+            cache[k][0]++;
+        if (++i==1000)
+            i=0;
+        uint64_t v=the1000[i];
+        CHECK(hm_get(c, v) == (void*)v);
+        count++;
+    }
+    return (void*)count;
+}
+
 static void* thread_write1000_cachekiller(void* c)
 {
     volatile uint64_t cache[CACHESIZE][8];
@@ -167,128 +185,37 @@ static void* thread_write1000_cachekiller(void* c)
 /* tests */
 /*********/
 
-static void test_read1()
+typedef void *(*thread_func_t)(void *);
+
+static void run_test(int spreload, int rpreload, thread_func_t rthread, thread_func_t wthread)
 {
     void *c = hm_new();
-    hm_insert(c, K, (void*)K);
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1_of_2()
-{
-    void *c = hm_new();
-    hm_insert(c, K, (void*)K);
-    hm_insert(c, 1, (void*)1);
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1_of_1000()
-{
-    void *c = hm_new();
-    hm_insert(c, K, (void*)K);
-    for (int i=0; i<999; i++)
+    if (spreload>=1)
+        hm_insert(c, K, (void*)K);
+    if (spreload>=2)
+        hm_insert(c, 1, (void*)1);
+    for (int i=spreload; i<rpreload; i++)
         hm_insert(c, the1000[i], (void*)the1000[i]);
 
-    pthread_t th[nthreads];
+    pthread_t th[nthreads], wr[nwthreads];
+    int ntr=wthread?nrthreads:nthreads;
+    int ntw=wthread?nwthreads:0;
     done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1000_of_1000()
-{
-    void *c = hm_new();
-    for (int i=0; i<1000; i++)
-        hm_insert(c, the1000[i], (void*)the1000[i]);
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1000_of_1000, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1_write1000()
-{
-    void *c = hm_new();
-    hm_insert(c, K, (void*)K);
-
-    pthread_t th[nrthreads], wr[nwthreads];
-    done=0;
-    for (int i=0; i<nrthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1, c));
-    for (int i=0; i<nwthreads; i++)
-        CHECK(!pthread_create(&wr[i], 0, thread_write1000, c));
+    for (int i=0; i<ntr; i++)
+        CHECK(!pthread_create(&th[i], 0, rthread, c));
+    for (int i=0; i<ntw; i++)
+        CHECK(!pthread_create(&wr[i], 0, wthread, c));
     sleep(1);
     done=1;
 
     uint64_t countr=0, countw=0;
-    for (int i=0; i<nrthreads; i++)
+    for (int i=0; i<ntr; i++)
     {
         void* retval;
         CHECK(!pthread_join(th[i], &retval));
         countr+=(uintptr_t)retval;
     }
-    for (int i=0; i<nwthreads; i++)
+    for (int i=0; i<ntw; i++)
     {
         void* retval;
         CHECK(!pthread_join(wr[i], &retval));
@@ -296,174 +223,21 @@ static void test_read1_write1000()
     }
 
     hm_delete(c);
-    printf("\e[F\e[40C%15lu %15lu\n", countr, countw);
+    if (ntw)
+        printf("\e[F\e[40C%15lu %15lu\n", countr, countw);
+    else
+        printf("\e[F\e[40C%15lu\n", countr);
 }
 
-static void test_read1000_write1000()
-{
-    void *c = hm_new();
-    for (int i=0; i<1000; i++)
-        hm_insert(c, the1000[i], (void*)the1000[i]);
-
-    pthread_t th[nrthreads], wr[nwthreads];
-    done=0;
-    for (int i=0; i<nrthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1000_of_1000, c));
-    for (int i=0; i<nwthreads; i++)
-        CHECK(!pthread_create(&wr[i], 0, thread_write1000, c));
-    sleep(1);
-    done=1;
-
-    uint64_t countr=0, countw=0;
-    for (int i=0; i<nrthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        countr+=(uintptr_t)retval;
-    }
-    for (int i=0; i<nwthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(wr[i], &retval));
-        countw+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu %15lu\n", countr, countw);
-}
-
-static void test_read_write_remove()
-{
-    void *c = hm_new();
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read_write_remove, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1_cachekiller()
-{
-    void *c = hm_new();
-    hm_insert(c, K, (void*)K);
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1_cachekiller, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void* thread_read1000_of_1000_cachekiller(void* c)
-{
-    volatile uint64_t cache[CACHESIZE][8];
-
-    uint64_t count=0;
-    int i=0;
-    while (!done)
-    {
-        for (int k=0; k<CACHESIZE; k++)
-            cache[k][0]++;
-        if (++i==1000)
-            i=0;
-        uint64_t v=the1000[i];
-        CHECK(hm_get(c, v) == (void*)v);
-        count++;
-    }
-    return (void*)count;
-}
-
-static void test_read1000_of_1000_cachekiller()
-{
-    void *c = hm_new();
-    for (int i=0; i<1000; i++)
-        hm_insert(c, the1000[i], (void*)the1000[i]);
-
-    pthread_t th[nthreads];
-    done=0;
-    for (int i=0; i<nthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1000_of_1000_cachekiller, c));
-    sleep(1);
-    done=1;
-
-    uint64_t count=0;
-    for (int i=0; i<nthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        count+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu\n", count);
-}
-
-static void test_read1000_write1000_cachekiller()
-{
-    void *c = hm_new();
-    for (int i=0; i<1000; i++)
-        hm_insert(c, the1000[i], (void*)the1000[i]);
-
-    pthread_t th[nrthreads], wr[nwthreads];
-    done=0;
-    for (int i=0; i<nrthreads; i++)
-        CHECK(!pthread_create(&th[i], 0, thread_read1000_of_1000_cachekiller, c));
-    for (int i=0; i<nwthreads; i++)
-        CHECK(!pthread_create(&wr[i], 0, thread_write1000_cachekiller, c));
-    sleep(1);
-    done=1;
-
-    uint64_t countr=0, countw=0;
-    for (int i=0; i<nrthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(th[i], &retval));
-        countr+=(uintptr_t)retval;
-    }
-    for (int i=0; i<nwthreads; i++)
-    {
-        void* retval;
-        CHECK(!pthread_join(wr[i], &retval));
-        countw+=(uintptr_t)retval;
-    }
-
-    hm_delete(c);
-    printf("\e[F\e[40C%15lu %15lu\n", countr, countw);
-}
-
-static void run_test(void (*func)(void), const char *name, int mut)
+static void test(const char *name, int spreload, int rpreload,
+    thread_func_t rthread, thread_func_t wthread)
 {
     printf("TEST: %s\n", name);
 
     for (int i=0; i<ARRAYSZ(hms); i++)
     {
         hm_select(i);
-        if (mut && hm_immutable)
+        if (wthread && hm_immutable)
         {
             printf(" \e[35m[\e[1m!\e[22m]\e[0m: %s\n", hm_name);
             continue;
@@ -471,14 +245,13 @@ static void run_test(void (*func)(void), const char *name, int mut)
 
         printf(" \e[34m[\e[1m⚒\e[22m]\e[0m: %s\n", hm_name);
         bad=0;
-        func();
+        run_test(spreload, rpreload, rthread, ((intptr_t)wthread==-1)?0:wthread);
         if (!bad)
             printf("\e[F \e[32m[\e[1m✓\e[22m]\e[0m\n");
         else
             printf("\e[F \e[31m[\e[1m✗\e[22m]\e[0m\n"), any_bad=1;
     }
 }
-#define TEST(x,mut) do run_test(test_##x, #x, mut); while (0)
 
 int main()
 {
@@ -496,15 +269,15 @@ int main()
         nrthreads = 1;
     printf("Using %lu threads; %lu readers %lu writers in mixed tests.\n",
         nthreads, nrthreads, nwthreads);
-    TEST(read1, 0);
-    TEST(read1_of_2, 0);
-    TEST(read1_of_1000, 0);
-    TEST(read1000_of_1000, 0);
-    TEST(read1_write1000, 1);
-    TEST(read1000_write1000, 1);
-    TEST(read_write_remove, 1);
-    TEST(read1_cachekiller, 0);
-    TEST(read1000_of_1000_cachekiller, 0);
-    TEST(read1000_write1000_cachekiller, 1);
+    test("read 1-of-1", 1, 0, thread_read1, 0);
+    test("read 1-of-2", 2, 0, thread_read1, 0);
+    test("read 1-of-1000", 1, 1000, thread_read1, 0);
+    test("read 1000-of-1000", 0, 1000, thread_read1000, 0);
+    test("read 1 write 1000", 1, 0, thread_read1, thread_write1000);
+    test("read 1000 write 1000", 0, 1000, thread_read1000, thread_write1000);
+    test("read-write-remove", 0, 0, thread_read_write_remove, (thread_func_t)-1);
+    test("read 1-of-1 cachekiller", 1, 0, thread_read1_cachekiller, 0);
+    test("read 1-of-1000 cachekiller", 1, 1000, thread_read1_cachekiller, 0);
+    test("read 1000 write 1000 cachekiller", 0, 1000, thread_read1000_cachekiller, thread_write1000_cachekiller);
     return any_bad;
 }
