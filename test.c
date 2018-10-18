1bd63e12d574b7a043974c020d53851d0951ad45
Adam Borowski <kilobyte@angband.pl>
2018-10-18 16:38:53 +0200

    Make a test output more readable.

--- test.c
+++ test.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-    HM_SELECT(tcradix_mutex4);
+    HM_SELECT(critbit);
     void *c = hm_new();
     hm_insert(c, 122, "abc");
     hm_insert(c, 123, "def");
     hm_insert(c, 0x01000000, "zzz");
 
-    printf("122 = %016lx\n", (uintptr_t)hm_get(c, 122));
-    printf("123 = %016lx\n", (uintptr_t)hm_get(c, 123));
-    printf("124 = %016lx\n", (uintptr_t)hm_get(c, 124));
+    printf("122 = %s\n", (const char*)hm_get(c, 122));
+    printf("123 = %s\n", (const char*)hm_get(c, 123));
+    printf("124 = %s\n", (const char*)hm_get(c, 124));
 
-    printf("122 =- %016lx\n", (uintptr_t)hm_remove(c, 122));
-    printf("123 =- %016lx\n", (uintptr_t)hm_remove(c, 123));
-    printf("124 =- %016lx\n", (uintptr_t)hm_remove(c, 124));
+    printf("122 =- %s\n", (const char*)hm_remove(c, 122));
+    printf("123 =- %s\n", (const char*)hm_remove(c, 123));
+    printf("124 =- %s\n", (const char*)hm_remove(c, 124));
     hm_remove(c, 0x01000000);
 
     hm_delete(c);
