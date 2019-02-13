4b39cbd6ae4c2527f0679574337207c6f6a3366a
Adam Borowski <kilobyte@angband.pl>
2019-02-13 16:32:22 +0100

    Test llugin's bug.

--- test.c
+++ test.c
@@ -3,29 +3,12 @@
 
 int main()
 {
-    HM_SELECT(critnib_tag);
+    HM_SELECT(critnib);
     void *c = hm_new();
-    hm_insert(c, 0x1, "1");
-    hm_insert(c, 0x0, "0");
-    hm_insert(c, 0x2, "2");
-#define GET(x) printf("get(%lx) → %s\n", (uint64_t)(x), (const char*)hm_get(c, (x)))
-    GET(122);
-    GET(0);
-    GET(1);
-    GET(2);
-    GET(3);
-    GET(4);
-    GET(5);
-    GET(0x11);
-    GET(0x12);
-#define LE(x) printf("le(%lx) → %s\n", (uint64_t)(x), (const char*)hm_find_le(c, (x)))
-    LE(1);
-    LE(2);
-    LE(5);
-    LE(6);
-    LE(0x11);
-    LE(0x15);
-    LE(0xfffffff);
+
+    hm_insert(c, 0xdf19b817a2a574f0, "a");
+    hm_insert(c, 0xc624b394880c9216, "c");
+    hm_remove(c, 0xb2dfb9f1c6c36aa2);
 
     hm_delete(c);
     return 0;
